90282818fe71c8ec579f021974394c1da516f998
Anronton <anton.eneroth@gmail.com>
2023-03-06 16:21:29 +0100

    Now the game plays with a predetermined player input, we also ave some limited output in the console.log()

--- script.js
+++ script.js
@@ -1,34 +1,41 @@
 
 
-
-//*let playerSelection = prompt("Rock, Scissors or paper?")*/
-   
-
-        
-
-
         function getComputerChoice(){
             const randNum = Math.floor(Math.random() * 3) + 1;
 
             switch(randNum){
                 case 1:
-                    return "rock";
+                    return "Rock";
                     break;
                 case 2: 
-                    return "paper"
+                    return "Paper"
                     break;
                 case 3: 
-                    return "scissors"   
+                    return "Scissors"   
             }
         }
 
-        function playRound(playerSelection, computerSelection) {
+        let playerSelection = "Rock";
+        const computerSelection = getComputerChoice();
 
+
+        function playRound(playerSelection, computerSelection) {
+        
+            if ((playerSelection == "rock" && computerSelection == "scissors") || 
+            (playerSelection == "scissors" && computerSelection == "paper") ||
+            (playerSelection == "paper" && computerSelection == "rock")) {
+                return `You Win, ${playerSelection} beats ${computerSelection}!`
+            }
+            else if (playerSelection == computerSelection) {
+                return "It's a tie!"
+            } else {
+                return `You Lose, ${computerSelection} beats ${playerSelection}!`
+            }
         }
         
 
          
-
-        const playerSelection = "Rock";
-        const computerSelection = getComputerChoice();
         console.log(playRound(playerSelection, computerSelection));
+
+        //*function game();*/
+        
